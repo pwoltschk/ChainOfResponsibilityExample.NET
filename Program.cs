9e3a5ce16ea22d369a369f98ee05d8fc2cbae576
wopa <p.woltschkow@gmail.com>
2020-01-04 21:22:21 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Transaction_processing.BL.Handlers.TransactionHandlers;
+using Transaction_processing.BL.Models;
+using System;
+
+namespace Transaction_processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
