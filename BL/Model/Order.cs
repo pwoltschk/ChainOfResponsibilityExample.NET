206f6cdc7454b33c6a55a38f31a8e6e72c316408
wopa <p.woltschkow@gmail.com>
2020-01-04 21:35:59 +0200

    Create Models

---
+++ BL/Model/Order.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TransactionProcessing.BL.Model
+{
+    public class Order
+    {
+        public Dictionary<Item, int> LineItems { get; } = new Dictionary<Item, int>();
+
+        public IList<Transaction> SelectedTransactions { get; } = new List<Transaction>();
+
+        public IList<Transaction> CompletedTransactions { get; } = new List<Transaction>();
+
+        public decimal AmountDue => LineItems.Sum(item => item.Key.Price * item.Value) - CompletedTransactions.Sum(payment => payment.Amount);
+
+    }
+
+    public enum TransactionProvider
+    {
+        Paypal,
+        Card,
+        Invoice
+    }
+
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public TransactionProvider TransactionProvider { get; set; }
+    }
+
+    public class Item
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Item(string id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
