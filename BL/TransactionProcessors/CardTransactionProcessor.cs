bdd27d3b700515f098bf4adc920fc0b56d237f78
wopa <p.woltschkow@gmail.com>
2020-01-05 18:36:31 +0200

    Create Transaction processor for card payments

---
+++ BL/TransactionProcessors/CardTransactionProcessor.cs
@@ -0,0 +1,19 @@
+﻿using TransactionProcessing.BL.Model;
+using System.Linq;
+
+namespace TransactionProcessing.BL.TransactionProcessors
+{
+    public class CardTransactionProcessor : ITransactionProcessor
+    {
+        public void Complete(Order order)
+        {
+            // Invoke the Stripe API
+            var payment = order.SelectedTransactions
+                .FirstOrDefault(x => x.TransactionProvider == TransactionProvider.Card);
+
+            if (payment == null) return;
+
+            order.CompletedTransactions.Add(payment);
+        }
+    }
+}
