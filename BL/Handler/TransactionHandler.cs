2b9dd932e5fc6db3220bd165c1b6af66f10a1e99
wopa <p.woltschkow@gmail.com>
2020-01-05 19:37:27 +0200

    create transaction handler

---
+++ BL/Handler/TransactionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TransactionProcessing.BL.Model;
+
+namespace TransactionProcessing.BL.Handlers.TransactionHandlers
+{
+    public abstract class TransactionHandler : IHandler<Order>
+    {
+        private IHandler<Order> Next { get; set; }
+
+        public virtual void Handle(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHandler<Order> SetNext(IHandler<Order> next)
+        {
+            Next = next;
+
+            return Next;
+        }
+    }
+}
