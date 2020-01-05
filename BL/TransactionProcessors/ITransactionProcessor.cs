c2ef9eab0ad992e4ad02a67cca1222fb7c334f67
wopa <p.woltschkow@gmail.com>
2020-01-05 18:36:31 +0200

    Create Transaction processor for card payments

---
+++ BL/TransactionProcessors/ITransactionProcessor.cs
@@ -0,0 +1,9 @@
+﻿using TransactionProcessing.BL.Model;
+
+namespace TransactionProcessing.BL.TransactionProcessors
+{
+    public interface ITransactionProcessor
+    {
+        void Complete(Order order);
+    }
+}
