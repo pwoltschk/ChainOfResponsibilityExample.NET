f0f591eff4b03f62780c394f0fa8fae896e81875
wopa <p.woltschkow@gmail.com>
2020-01-09 16:46:32 +0200

    Added paypal dummy transaction

---
+++ BL/TransactionProcessors/PaypalTransactionProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using TransactionProcessing.BL.Model;
+
+namespace TransactionProcessing.BL.TransactionProcessors
+{
+    public class PaypalTransactionProcessor : ITransactionProcessor
+    {
+        public void Complete(Order order)
+        {
+            var payment = order.SelectedTransactions
+                .FirstOrDefault(x => x.TransactionProvider == TransactionProvider.Paypal);
+
+            if (payment == null) return;
+
+            order.CompletedTransactions.Add(payment);
+        }
+    }
+}
