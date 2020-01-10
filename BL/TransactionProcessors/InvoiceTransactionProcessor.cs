d25bb227ed23a9882331ee2e9cf078a427cca763
wopa <p.woltschkow@gmail.com>
2020-01-10 19:38:12 +0200

    Invoice transaction

---
+++ BL/TransactionProcessors/InvoiceTransactionProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using TransactionProcessing.BL.Model;
+
+namespace TransactionProcessing.BL.TransactionProcessors
+{
+    public class InvoiceTransactionProcessor : ITransactionProcessor
+    {
+        public void Complete(Order order)
+        {
+            var payment = order.SelectedTransactions
+                .FirstOrDefault(x => x.TransactionProvider == TransactionProvider.Invoice);
+
+            if (payment == null) return;
+
+            order.CompletedTransactions.Add(payment);
+        }
+    }
+}
