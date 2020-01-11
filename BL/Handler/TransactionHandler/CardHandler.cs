2b1de4c9d43ead87281f1aeff0211d7cc22d7f52
wopa <p.woltschkow@gmail.com>
2020-01-11 15:13:44 +0200

    Implement Card Handler

--- BL/Handler/TransactionHandler/CardHandler.cs
+++ BL/Handler/TransactionHandler/CardHandler.cs
@@ -1,7 +1,22 @@
-﻿namespace TransactionProcessing.BL.Handlers.TransactionHandlers
+﻿using TransactionProcessing.BL.Model;
+using TransactionProcessing.BL.TransactionProcessors;
+using System.Linq;
+
+namespace TransactionProcessing.BL.Handlers.TransactionHandlers
 {
     public class CardHandler : TransactionHandler
     {
-        /// TODO: Implement me
+        public CardTransactionProcessor CardTransactionProcessor { get; }
+            = new CardTransactionProcessor();
+
+        public override void Handle(Order order)
+        {
+            if (order.SelectedTransactions.Any(x => x.TransactionProvider == TransactionProvider.Card))
+            {
+                CardTransactionProcessor.Complete(order);
+            }
+
+            base.Handle(order);
+        }
     }
 }
