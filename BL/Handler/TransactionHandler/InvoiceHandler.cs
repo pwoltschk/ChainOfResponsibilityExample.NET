ee1a1f30e7a22990f7780df2744f8cf3266c6c89
wopa <p.woltschkow@gmail.com>
2020-01-11 15:24:05 +0200

    implemented invoice and paypal handler

--- BL/Handler/TransactionHandler/InvoiceHandler.cs
+++ BL/Handler/TransactionHandler/InvoiceHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace TransactionProcessing.BL.Handlers.TransactionHandlers
+﻿using TransactionProcessing.BL.Model;
+using TransactionProcessing.BL.TransactionProcessors;
+using System.Linq;
+
+namespace TransactionProcessing.BL.Handlers.TransactionHandlers
 {
     public class InvoiceHandler : TransactionHandler
     {
+        public InvoiceTransactionProcessor InvoiceTransactionProcessor { get; }
+            = new InvoiceTransactionProcessor();
+
+        public override void Handle(Order order)
+        {
+            if (order.SelectedTransactions.Any(x => x.TransactionProvider == TransactionProvider.Invoice))
+            {
+                InvoiceTransactionProcessor.Complete(order);
+            }
+            base.Handle(order);
+        }
     }
 }
--- BL/Handler/TransactionHandler/PaypalHandler.cs
+++ BL/Handler/TransactionHandler/PaypalHandler.cs
@@ -1,6 +1,22 @@
-﻿namespace TransactionProcessing.BL.Handlers.TransactionHandlers
+﻿using TransactionProcessing.BL.Model;
+using TransactionProcessing.BL.TransactionProcessors;
+using System.Linq;
+
+namespace TransactionProcessing.BL.Handlers.TransactionHandlers
 {
     public class PaypalHandler : TransactionHandler
     {
+        private PaypalTransactionProcessor PaypalTransactionProcessor { get; }
+            = new PaypalTransactionProcessor();
+
+        public override void Handle(Order order)
+        {
+            if (order.SelectedTransactions.Any(x => x.TransactionProvider == TransactionProvider.Paypal))
+            {
+                PaypalTransactionProcessor.Complete(order);
+            }
+
+            base.Handle(order);
+        }
     }
 }
