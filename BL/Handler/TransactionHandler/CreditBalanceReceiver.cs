ab494b5507f2489740c725cb820753bd690ec6c9
wopa <p.woltschkow@gmail.com>
2020-02-08 15:51:14 +0200

    added balance handler

---
+++ BL/Handler/TransactionHandler/CreditBalanceReceiver.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using TransactionProcessor.BL.Model;
+using TransactionProcessor.BL.TransactionProcessors;
+
+namespace TransactionProcessor.BL.Handler.TransactionHandler
+{
+    internal class CreditBalanceReceiver : IReceiver<Order>
+    {
+        public readonly CreditBalanceTransactionProcessor _balanceTransactionProcessor;
+
+        public CreditBalanceReceiver(CreditBalanceTransactionProcessor balanceTransactionProcessor)
+        {
+            _balanceTransactionProcessor = balanceTransactionProcessor;
+        }
+
+        public void Handle(Order order)
+        {
+            if (order.SelectedTransactions.Any(x => x.TransactionProvider == TransactionProvider.Card))
+            {
+                _balanceTransactionProcessor.Complete(order);
+            }
+        }
+    }
+}
---
+++ BL/TransactionProcessors/CreditBalanceTransactionProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using TransactionProcessor.BL.Model;
+
+namespace TransactionProcessor.BL.TransactionProcessors
+{
+    public class CreditBalanceTransactionProcessor : ITransactionProcessor
+    {
+        public void Complete(Order order)
+        {
+            var payment = order.SelectedTransactions
+                .FirstOrDefault(x => x.TransactionProvider == TransactionProvider.Invoice);
+
+            if (payment == null) return;
+
+            order.CompletedTransactions.Add(payment);
+        }
+    }
+}
